252c5a5e163db9e0de24b4d2ab2aec5c832514e5
wunter2018 <17826155520@163.com>
2019-12-18 10:36:49 +0800

    Update main.c

--- bsp/stm32f10x/applications/main.c
+++ bsp/stm32f10x/applications/main.c
@@ -12,43 +12,56 @@
 #include <rtdevice.h>
 #include <board.h>
 
-static char rmt_send_pool[256];
-static struct rt_mailbox rmt_send;
+#define		MSG_GETINFO 	0x0000
+#define 	MSG_COAPSEND 	0x1000
+
+	
+static char mb_control_pool[256];
+static struct rt_mailbox mb_control;
+static char mb_coap_pool[256];
+static struct rt_mailbox mb_coap;
 
 struct AppMsg
 {
 	int msg_type;
+	int msg_payload;
 };
 typedef struct AppMsg AppMsg_t;
 
+static AppMsg_t * notifyMsg;
+
+void notify(AppMsg_t *appmsg);
+void coap_send(AppMsg_t *appmsg);
+void send_by_uart2(const char * str);
+
 void timeout1(void *parameter)
 {
-	static int tick1=0;
-	rt_kprintf("time1 %d\n",tick1++);
-}
-
-void timeout2(void *parameter)
-{
-	static int tick2=0;
-	rt_kprintf("time2 %d\n",tick2++);
+	notifyMsg->msg_type = MSG_GETINFO;
+	notify(notifyMsg);
 }
 
 void control_thread_entry(void *parameter)
 {
 	AppMsg_t * appMsg;
+	AppMsg_t * sendMsg;
+	static int tick;
 	while(1)
 	{
-		if(rt_mb_recv(&rmt_send, (rt_ubase_t *)&appMsg, RT_WAITING_FOREVER)== RT_EOK)
+		if(rt_mb_recv(&mb_control, (rt_ubase_t *)&appMsg, RT_WAITING_FOREVER)== RT_EOK)
 		{
-			rt_kprintf("msg:%d\n",appMsg->msg_type);
 			switch(appMsg->msg_type)
 			{
-				case 2:
-					rt_kprintf("I get message\n");
+				case MSG_GETINFO:
+					sendMsg = (AppMsg_t *)rt_malloc(sizeof(AppMsg_t));
+					rt_kprintf("Control get message %d\n",tick);
+				
+					sendMsg->msg_type = MSG_COAPSEND;
+					sendMsg->msg_payload = tick++;
+					coap_send(sendMsg);
 					break;
 				
 				default:
-					rt_kprintf("invalid Message type!\n");
+					rt_kprintf("Control Invalid Message!\n");
 			}
 		}
 	}
@@ -56,41 +69,93 @@ void control_thread_entry(void *parameter)
 
 void coap_thread_entry(void *parameter)
 {
+	AppMsg_t * appMsg;
+	char message[50];
 	while(1)
 	{
-		rt_kprintf("this is coap thread\n");
-		rt_thread_delay(200);
+		if(rt_mb_recv(&mb_coap, (rt_ubase_t *)&appMsg, RT_WAITING_FOREVER)== RT_EOK)
+		{
+			switch(appMsg->msg_type)
+			{
+				case MSG_COAPSEND:
+					rt_sprintf(message,"Coap get message %d\n",appMsg->msg_payload);
+					send_by_uart2(message);
+					break;
+				
+				default:
+					rt_kprintf("Coap Invalid Message!\n");
+			}
+		}
 	}
 }
 
 int main(void)
 {
-    rt_timer_t rtt1 = rt_timer_create("Time1", timeout1, RT_NULL, 1000, RT_TIMER_FLAG_PERIODIC);
-	rt_timer_t rtt2 = rt_timer_create("Time2", timeout2, RT_NULL, 400, RT_TIMER_FLAG_PERIODIC);
+	rt_err_t result;
 	
-	rt_err_t result = rt_mb_init(&rmt_send,"Send Box", &rmt_send_pool, sizeof(rmt_send_pool)/4, RT_IPC_FLAG_FIFO);
+	notifyMsg = (AppMsg_t *)rt_malloc(sizeof(AppMsg_t));
+	
+	/*´´Ôì Timer, Thread, MaiBox*/
+	
+    rt_timer_t rtt1 = rt_timer_create("Time1", timeout1, RT_NULL, 200, RT_TIMER_FLAG_PERIODIC);
+	
+	result = rt_mb_init(&mb_control,"MailBox Control", &mb_control_pool, sizeof(mb_control_pool)/4, RT_IPC_FLAG_FIFO);
 	if(result != RT_NULL)
-		rt_kprintf("Send Mainbox create fail\n");
+		rt_kprintf("MailBox Control create fail\n");
+	
+	result = rt_mb_init(&mb_coap,"MailBox Coap", &mb_coap_pool, sizeof(mb_coap_pool)/4, RT_IPC_FLAG_FIFO);
+	if(result != RT_NULL)
+		rt_kprintf("MailBox Coap create fail\n");
+	
 	
 	rt_thread_t rtd_control = rt_thread_create("Main Thread", control_thread_entry, RT_NULL, 1024, 12, 100);
+	
 	rt_thread_t rtd_coap = rt_thread_create("Coap Thread",coap_thread_entry,  RT_NULL, 1024, 12, 50);
 	 
+	/*Æô¶¯*/
 	
-	//rt_timer_start(rtt1);
-	//rt_timer_start(rtt2);
+	rt_timer_start(rtt1);
+	
 	if(rtd_control != RT_NULL)
 		rt_thread_startup(rtd_control);
-	//if(rtd_coap != RT_NULL)
-		//rt_thread_startup(rtd_coap);
+	
+	if(rtd_coap != RT_NULL)
+		rt_thread_startup(rtd_coap);
 	
     return RT_EOK;
 }
 
-void mySend(void)
+void notify(AppMsg_t *appmsg)
 {
-	AppMsg_t *appmsg = (AppMsg_t *)rt_malloc(sizeof(AppMsg_t));
-	appmsg->msg_type=2;
-	rt_kprintf("appmsg:%d\n",appmsg->msg_type);
-	rt_err_t result = rt_mb_send(&rmt_send,(rt_uint32_t)appmsg);
+	rt_err_t result = rt_mb_send(&mb_control,(rt_uint32_t)appmsg);
+	if(result != RT_NULL)
+		rt_kprintf("notify fail\n");
 }
-MSH_CMD_EXPORT_ALIAS(mySend, sendmsg, send message);
+
+void coap_send(AppMsg_t *appmsg)
+{
+	rt_err_t result = rt_mb_send(&mb_coap,(rt_uint32_t)appmsg);
+	if(result != RT_NULL)
+		rt_kprintf("coap send fail\n");
+}
+
+void send_by_uart2(const char * str)
+{
+	const char * tmp = str;
+	char amount=0;
+	
+	while(*(tmp++) != '\n' && amount<50 )
+		amount++;
+	
+	rt_device_t uart2 = rt_device_find("uart2");
+	if(uart2 != RT_NULL)
+	{
+		rt_err_t result = rt_device_open(uart2,RT_DEVICE_FLAG_INT_TX);
+		if(result != RT_NULL)
+		{
+			rt_kprintf("uart2 open fail\n");
+			return;
+		}
+		rt_device_write(uart2,0,str,amount+1);
+	}
+}
